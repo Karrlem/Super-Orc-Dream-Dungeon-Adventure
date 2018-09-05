54a3636accda90f2e2a4fb42539465b10923bcbf
ginaienopoli <gienopoli@aol.com>
2018-09-05 17:36:54 -0400

    PlayerDamage Start

--- Super Orc Dream Dungeon Adventure/Assets/Script/PlayerDamage.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Script/PlayerDamage.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour {
+    public bool isDamaging;
+    public float damage = 10f;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void OnTriggerStay(Collider col)
+    {
+        if(col.tag == "Player")
+            col.SendMessage((isDamaging)?"TakeDamage":"HealDamage",Time.deltaTime * damage);
+    }
 }
