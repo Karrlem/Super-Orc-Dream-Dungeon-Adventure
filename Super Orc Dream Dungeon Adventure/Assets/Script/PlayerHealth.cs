28f788e82ccefe759a02e0c9031e551d24bb786e
ginaienopoli <gienopoli@aol.com>
2018-09-05 16:25:24 -0400

    Added Health Bar Reduction
    fuck yeah

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+	public static int health = 100;
+    public Slider healthBar;
+
+
+    // Use this for initialization
+    void Start () {
+        healthBar = GetComponent <Slider> ();
+        healthBar.value = health;
+    }
+
+	
+    void ReduceHealth(){
+        health = health - 10;
+        healthBar.value = health;
+    }
+
+	void Update (){
+	if(Input.GetKeyDown (KeyCode.A)){
+		ReduceHealth();
+
+		}
+	}
+
+}
