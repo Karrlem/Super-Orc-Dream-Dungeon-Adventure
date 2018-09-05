3b915e5c8cf4a1e4aa3b7fd3c9ab2ef8c1e8a961
ginaienopoli <gienopoli@aol.com>
2018-09-04 20:12:25 -0400

    Added Scenes and Scripts
    Move and Jump

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/BasicJump.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicJump : MonoBehaviour {
+
+	public float jump = 10;
+
+		void Update (){
+		if(Input.GetKeyDown (KeyCode.Space)){
+		if(transform.position.y <= 1.0f ){
+			GetComponent<Rigidbody> ().AddForce (Vector3.up * jump);
+			}
+		}
+	}
+}
---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/BasicMove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMove : MonoBehaviour {
+
+	public float speed = 50;
+	private Rigidbody rb;
+
+	void Start(){
+		rb = GetComponent <Rigidbody> ();
+	}
+
+	void FixedUpdate(){
+		float hMove = Input.GetAxis ("Horizontal");
+		float vMove = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (hMove, 0f, vMove);
+
+		rb.AddForce (movement * speed);
+	}
+}
