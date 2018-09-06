9428d313ebbe8b1b8ba94b41da6d142049362773
Karrlem Lewis <karrlemlewis@yahoo.com>
2018-09-06 16:39:14 -0400

    Finished Health and dyingfor player

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/DamageScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class DamageScript : MonoBehaviour {
+
+	
+	float damage = 50f;
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("ouchies");
+		other.gameObject.GetComponent<HealthScript>().TakeDamage(damage);
+	}
+}
---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/HealthScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthScript : MonoBehaviour 
+{
+
+	//public Image healthBar;
+	public Slider healthBar;
+	public float max_health = 100f;
+	public float cur_health = 0f;
+	public bool alive = true;
+
+	// Use this for initialization
+	void Start () 
+	{
+		healthBar = GetComponent<Slider>();
+		healthBar.value = cur_health;
+		cur_health = max_health;
+		SetHealthBar ();
+	}
+
+	public void TakeDamage(float amount)
+	{
+		//checking if player is alive
+		if(!alive)
+		{
+			return;
+		}
+		if(cur_health <= 0)
+		{
+			//kill player
+			cur_health = 0;
+			alive = false;
+			gameObject.SetActive(false);
+		}
+		cur_health -= amount;
+		SetHealthBar ();
+	}
+
+	void SetHealthBar () 
+	{
+	
+		float my_health = cur_health / max_health;
+		healthBar.transform.localScale = new Vector3 (Mathf.Clamp (my_health, 0f, 1f),healthBar.transform.localScale.y,healthBar.transform.localScale.z);
+		
+
+	}
+}
