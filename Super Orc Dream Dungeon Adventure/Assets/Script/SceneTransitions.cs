434a75349bb9103464fe94932816aa8a7cf6b118
Karrlem <karrlemlewis@yahoo.com>
2018-09-14 13:33:34 -0400

    Cont. Transition code

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/SceneTransitions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneTransitions : MonoBehaviour
+{
+    public Animator transitionAnim;
+    public string sceneName;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+
+}
