0b13fe1589aaa89df0f2551b56561808fd099f4b
ginaienopoli <gienopoli@aol.com>
2018-09-13 20:25:18 -0400

    Collecting Items

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/CollectItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectItem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+//Freeze Y Poistion on collecting object
+	void OnTriggerEnter(Collider plyr)
+	{
+		if (plyr.gameObject.tag == "Player"){
+		Debug.Log("key wee");
+		Destroy(gameObject);
+		
+		}
+	}
+}
