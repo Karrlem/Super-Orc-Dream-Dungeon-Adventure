f47aaf2b6e368995ddf96f19a82a4f721afaab77
ginaienopoli <gienopoli@aol.com>
2018-09-20 17:28:17 -0400

    Main Menu Completed

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public string Prototype;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void NewGame(){
+		SceneManager.LoadScene(Prototype);
+	}
+
+	public void QuitGame(){
+		Application.Quit();
+	}
+}
