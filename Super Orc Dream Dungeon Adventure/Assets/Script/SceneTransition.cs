4d5160e09f66729b57d2b8d3a30bd2ad6fcfef03
Karrlem Lewis <karrlemlewis@yahoo.com>
2018-09-20 14:49:21 -0400

    Finished Scene Transitions
    Small bug where the scene lighting is darker.

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/SceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour 
+{
+	public Animator tranisitionAnim;
+	public string sceneName;
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			StartCoroutine(LoadScene());
+		}
+	}
+	IEnumerator LoadScene()
+	{
+		tranisitionAnim.SetTrigger("End");
+		yield return new WaitForSeconds(1.5f);
+		SceneManager.LoadScene(sceneName);
+	}
+}
