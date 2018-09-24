78964341a3405af57494baaaf9aeca82a50c5d44
Karrlem Lewis <karrlemlewis@yahoo.com>
2018-09-24 18:57:16 -0400

    Added Controllers Stats(Still working)

---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/CharacterStats.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class CharacterStats : MonoBehaviour 
+{
+	public int maxHealth = 100;
+	public int currentHealth { get; private set; }
+	public Stats damage;
+	public Stats armor;
+
+	void Awake()
+	{
+		currentHealth = maxHealth;
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.T))
+		{
+			TakeDamage(10);
+		}
+	}
+	public void TakeDamage(int damage)
+	{
+		damage -= armor.GetValue();
+		damage = Mathf.Clamp(damage, 0, int.MaxValue);	
+
+		currentHealth -= damage;
+		Debug.Log(transform.name + "takes" + damage + "damage");
+
+		if(currentHealth <= 0)
+		{
+			Die();
+		}
+	}
+	public virtual void Die()
+	{
+		//Die in some way
+		//This method is meant to be overwritten
+		Debug.Log(transform.name + "Died");
+	}
+}
---
+++ Super Orc Dream Dungeon Adventure/Assets/Script/Stats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Stats
+{
+	[SerializeField]
+	private int baseValue;
+
+	public int GetValue()
+	{
+		return baseValue;
+	}
+}
