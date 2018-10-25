b675106099b03fae90a46248f8afd9e33673a09a
Karrlem Lewis <karrlemlewis@yahoo.com>
2018-10-25 16:51:17 -0400

    Fixing code relating to attacking

--- Super Orc Dream Dungeon Adventure/Assets/Script/CharacterStats.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Script/CharacterStats.cs
@@ -1,41 +1,76 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
-public class CharacterStats : MonoBehaviour 
+public class CharacterStats : MonoBehaviour
 {
-	public int maxHealth = 100;
-	public int currentHealth { get; private set; }
-	public Stats damage;
-	public Stats armor;
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+    public Stats damage;
+    public Stats armor;
+    public bool canAttack = false;
 
-	void Awake()
-	{
-		currentHealth = maxHealth;
-	}
 
-	void Update()
-	{
-		if(Input.GetKeyDown(KeyCode.T))
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+
+        /*
+        if (canAttack == true)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                Destroy(gameObject);
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.T))
 		{
 			TakeDamage(10);
-		}
-	}
-	public void TakeDamage(int damage)
-	{
-		damage -= armor.GetValue();
-		damage = Mathf.Clamp(damage, 0, int.MaxValue);	
+		}*/
+    }
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
 
-		currentHealth -= damage;
-		Debug.Log(transform.name + "takes" + damage + "damage");
+        currentHealth -= damage;
+        Debug.Log(transform.name + "takes" + damage + "damage");
 
-		if(currentHealth <= 0)
-		{
-			Die();
-		}
-	}
-	public virtual void Die()
-	{
-		//Die in some way
-		//This method is meant to be overwritten
-		Debug.Log(transform.name + "Died");
-	}
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    public virtual void Die()
+    {
+        //Die in some way
+        //This method is meant to be overwritten
+        Debug.Log(transform.name + "Died");
+    }
+
+    public void CanAttack()
+    {
+        if (canAttack == true)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+
+                TakeDamage(10);
+            }
+        }
+    }
+
+    void OnTriggerStay(Collider enmy)
+    {
+        if (enmy.gameObject.tag == "Enemy")
+        {
+            CanAttack();
+            canAttack = true;
+            Debug.Log("can attack");
+
+        }
+    }
 }
--- Super Orc Dream Dungeon Adventure/Assets/Script/EnemyStats.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Script/EnemyStats.cs
@@ -2,12 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStats : CharacterStats 
+public class EnemyStats : MonoBehaviour
 {
+    public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+    public Stats damage;
+    public Stats armor;
+
+    void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            TakeDamage(10);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        damage -= armor.GetValue();
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+
+        currentHealth -= damage;
+        Debug.Log(transform.name + "takes" + damage + "damage");
+
+        if (currentHealth <= 0)
+        {
+            Die();
+            Destroy(gameObject);
+        }
+    }
+    public virtual void Die()
+    {
+        //Die in some way
+        //This method is meant to be overwritten
+        Debug.Log(transform.name + "Died");
+    }
+
+}
+
+
+
+/*{
+    //from the chaaracter stats script
 	public override void Die()
 	{
 		base.Die();
-
+        //detroys the enemy
 		Destroy(gameObject);
 	}
 }
+*/
