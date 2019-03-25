9a967611722ded1268b810ea9f8054a2dd97c1b0
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-25 19:16:11 -0400

    Updating Player script

--- Super Orc Dream Dungeon Adventure/Assets/Script/BasicMove.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Script/BasicMove.cs
@@ -5,18 +5,30 @@ public class BasicMove : MonoBehaviour {
 	//Place on player and add rigidbody to player
 
 	public float speed = 20;
-	private Rigidbody rb;
+    public float jump = 10;
+    private Rigidbody rb;
 
 	void Start(){
 		rb = GetComponent <Rigidbody> ();
 	}
 
-	void Update(){
-		float hMove = Input.GetAxis ("Horizontal");
-		float vMove = Input.GetAxis ("Vertical");
+    void Update()
+    {
+        //Walking
+        float hMove = Input.GetAxis("Horizontal");
+        float vMove = Input.GetAxis("Vertical");
 
-		Vector3 movement = new Vector3 (hMove, 0f, vMove);
+        Vector3 movement = new Vector3(hMove, 0f, vMove);
 
-		rb.AddForce (movement * speed);
-	}
+        rb.AddForce(movement * speed);
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (transform.position.y <= 1.0f)
+            {
+                GetComponent<Rigidbody>().AddForce(Vector3.up * jump);
+            }
+        }
+    }
 }
