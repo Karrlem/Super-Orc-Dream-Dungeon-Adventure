b59c4c6151f7e4858407c5d2476aa7c6e3d7564b
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-25 16:42:56 -0400

    Update?

---
+++ Super Orc Dream Dungeon Adventure/New Unity Project/Assets/Scripts/MoveMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoveMe : MonoBehaviour {
+	Rigidbody rb;
+	public float speed;
+
+
+	void Start(){
+		rb = GetComponent<Rigidbody>();
+
+	}
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		rb.AddForce(movement * speed);
+	}
+}
