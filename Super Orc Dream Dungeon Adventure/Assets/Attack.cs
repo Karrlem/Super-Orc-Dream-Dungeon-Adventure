254dbfef135901429482cc1aa1bbeb390b451267
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-29 18:37:42 -0400

    Trying to fix attack

--- Super Orc Dream Dungeon Adventure/Assets/Attack.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Attack.cs
@@ -6,6 +6,8 @@ public class Attack : MonoBehaviour
 {
 
     private Animator m_animator;
+    private GameObject[] myEnenmies;
+    public GameObject objToDestory;
 
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,23 @@ public class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        myEnenmies = GameObject.FindGameObjectsWithTag("Enemy");
+
+
         if (Input.GetMouseButtonDown(0))
         {
             Debug.Log("Attacking");
             m_animator.SetTrigger("attack");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Sword")
+        {
+            Debug.Log("Hit");
+            Destroy(objToDestory);
         }
     }
 }
