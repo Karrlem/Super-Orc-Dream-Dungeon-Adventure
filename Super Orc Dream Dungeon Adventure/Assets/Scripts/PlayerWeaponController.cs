7a44d9dece40dbfcd51ce03735023bb5aeb34722
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-30 11:44:07 -0400

    Update PlayerWeaponController.cs

--- Super Orc Dream Dungeon Adventure/Assets/Scripts/PlayerWeaponController.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Scripts/PlayerWeaponController.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PlayerWeaponController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject playerHand;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
