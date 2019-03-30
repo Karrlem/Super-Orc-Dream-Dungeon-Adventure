2bdc63af74304463c125930e23fcc8958467e0cf
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-30 11:43:43 -0400

    Working on attacking

---
+++ Super Orc Dream Dungeon Adventure/Assets/Scripts/Sword.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour
+{
+    private Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void PerformAttack()
+    {
+        animator.SetTrigger("Base_Attack");
+    }
+}
