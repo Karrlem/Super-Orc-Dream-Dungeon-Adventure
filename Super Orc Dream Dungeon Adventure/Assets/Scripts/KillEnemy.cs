6a4322d8f1db80ccc57f13d69d5ef4d0c059a003
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-03-31 00:16:44 -0400

    Still attempting

--- Super Orc Dream Dungeon Adventure/Assets/Scripts/KillEnemy.cs
+++ Super Orc Dream Dungeon Adventure/Assets/Scripts/KillEnemy.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class KillEnemy : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
+     {
+         if(other.gameObject.tag == "Sword")
+         {
+             Debug.Log("Attacking");
+             Destroy (gameObject);
+         }
+     }
+    
+    private void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Sword")
+        if (other.gameObject.tag.Equals("Player"))
         {
-            Debug.Log("Attacking");
-            Destroy (gameObject);
+            Destroy(other.gameObject);
         }
     }
 }
