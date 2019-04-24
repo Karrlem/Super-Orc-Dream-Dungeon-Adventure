c51b5de62dfb2f2c81d5afe898b9397b5403a380
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-04-24 17:59:56 -0400

    Coding

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/menuController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class menuController : MonoBehaviour
+{
+    public string startScene;
+    // Start is called before the first frame update
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startScene,LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
