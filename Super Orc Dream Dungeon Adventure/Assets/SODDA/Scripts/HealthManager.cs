66c7aea39d21456df4318b70982f1d8e3c6fc30d
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-04-24 11:48:31 -0400

    Dmg

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/HealthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void HurtPlayer(int damage)
+    {
+        currentHealth -= damage;
+    }
+
+    public void HealPlayer(int healAmount)
+    {
+        //makes sure the health doest go above max limit.
+        currentHealth += healAmount;
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+    }
+}
---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/HurtPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtPlayer : MonoBehaviour
+{
+    public int damageToGive = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            FindObjectOfType<HealthManager>().HurtPlayer(damageToGive);
+        }
+    }
+}
