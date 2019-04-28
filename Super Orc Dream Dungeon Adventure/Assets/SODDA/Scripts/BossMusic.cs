0f707fabada090ec65074b45829eb5d7c6d2c965
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-04-28 17:41:31 -0400

    fixed the music to play through each scene and cancle when needed.

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/BossMusic.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMusic : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Play Global
+    private static BossMusic instance = null;
+    public static BossMusic Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+    //Play Gobal End
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
