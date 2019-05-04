4c4be9a622dc42ecc55da65f6959b9a2b6766be4
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-05-04 18:27:04 -0400

    Added the music tweaking required per scene

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/PauseHealing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseHealing : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        HealingMusic.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
