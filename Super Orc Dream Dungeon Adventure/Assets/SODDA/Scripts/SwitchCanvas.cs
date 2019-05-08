460705b057630c1d23c77004deadc7ee05f2f150
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-05-08 17:03:10 -0400

    Button

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/SwitchCanvas.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SwitchCanvas : MonoBehaviour
+{
+    public GameObject OffCanvas;
+    public GameObject OnCanvas;
+    public GameObject FirstObject;
+
+    public void Switch()
+    {
+        OffCanvas.SetActive(true);
+        OnCanvas.SetActive(false);
+        GameObject.Find("EventSystem").GetComponent<EventSystem>().SetSelectedGameObject(FirstObject, null);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
