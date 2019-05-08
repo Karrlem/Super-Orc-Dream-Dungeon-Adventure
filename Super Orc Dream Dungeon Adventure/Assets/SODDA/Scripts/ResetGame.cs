08c7bc8b0bc44471a8a6a415e3e11b849664c251
Karrlem Lewis <karrlemlewis@yahoo.com>
2019-05-07 23:09:53 -0400

    reset code

---
+++ Super Orc Dream Dungeon Adventure/Assets/SODDA/Scripts/ResetGame.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+{
+    public Animator tranisitionAnim;
+    public string sceneName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (Input.GetButtonDown("Fire1"))
+        {
+            SceneManager.LoadScene(MainMenuScene)
+        }*/
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadScene());
+        }
+    }
+
+    IEnumerator LoadScene()
+    {
+        tranisitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene(sceneName);
+    }
+}
